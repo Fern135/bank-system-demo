ff7821f538fb2a06575a2c854b28ecd545d4df5e
dante.camblor@gmail.com <dante.camblor@gmail.com>
2023-03-02 21:09:48 -0500

    Update tests.py

--- server/login_signUp/tests.py
+++ server/login_signUp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RenderTests(TestCase):
+    def login_view(self):
+        url = reverse('api/controllers/user/login/')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
